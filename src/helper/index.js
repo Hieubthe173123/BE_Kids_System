66b502b264c280ad3610104dc3e5457aecaf61a8
PhamKien28 <134904000+PhamKien2803@users.noreply.github.com>
2025-06-24 12:23:36 +0700

    API Forgot, Verify, Reset

---
+++ src/helper/index.js
@@ -0,0 +1,26 @@
+// const Parent = require("../models/Parent");
+// const Teacher = require("../models/Teacher");
+// const Principal = require("../models/Principal");
+// const Admin = require("../models/Admin");
+
+const parentModel = require("../models/parentModel");
+
+async function findAccountByEmail(email) {
+    const models = [
+        { model: parentModel, role: "parent" },
+        // { model: Teacher, role: "teacher" },
+        // { model: Principal, role: "principal" },
+        // { model: Admin, role: "admin" },
+    ];
+
+    for (const { model } of models) {
+        const user = await model.findOne({ email }).populate("account");
+        if (user && user.account) {
+            return { account: user.account, email };
+        }
+    }
+
+    return null;
+}
+
+exports.findAccountByEmail = findAccountByEmail;
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { loginAccount, logoutAccount, getInformationAccount } = require('../controllers/authController');
+const { loginAccount, logoutAccount, getInformationAccount, resetPassword, verifyOTP, forgotPassword } = require('../controllers/authController');
 const verifyToken = require("../middlewares/verifyToken");
 
 
-router.post("/login",loginAccount);
+router.post("/login", loginAccount);
 router.post("/logout", verifyToken, logoutAccount);
-router.get("/infomation",verifyToken, getInformationAccount);
+router.get("/infomation", verifyToken, getInformationAccount);
+
+router.post("/reset-password", resetPassword);
+router.post("/verify-otp", verifyOTP);
+router.post("/forgot-password", forgotPassword);
 
 module.exports = router;
