4533e969705146055f583dfcd2c417111905ca4e
havancuong2003 <havancuong07122003@gmail.com>
2025-06-26 20:14:14 +0700

    fix: fix authenticate, fix login controller

--- src/constants/useConstants.js
+++ src/constants/useConstants.js
@@ -38,8 +38,8 @@ const VALIDATION_CONSTANTS = {
 }
 
 const TOKEN = {
-    EXPIRESIN_TOKEN: '1d',
-    EXPIRESIN_REFESH_TOKEN: '1d',
+    EXPIRESIN_TOKEN: '1m',
+    EXPIRESIN_REFESH_TOKEN: '5m',
     EX: 7 * 24 * 60 * 60 
 }
 
