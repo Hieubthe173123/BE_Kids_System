5b10de21f9bc4385500b051c9fa54abec13e4dcf
dev.ttcn <dev.ttcn@gmail.com>
2025-07-13 00:59:58 +0700

    cho api vao controller theo Leader yeu cau

--- src/routes/studentRoute.js
+++ src/routes/studentRoute.js
@@ -4,21 +4,9 @@ const { findAllGeneric, findIdGeneric, createGeneric, deletedSoftGeneric, update
 const Student = require("../models/studentModel");
 const Parent = require("../models/parentModel");
 const verifyToken = require("../middlewares/verifyToken");
+const { getStudentsWithoutParents } = require("../controllers/studentController");
 
-router.get("/no-parent", async (req, res) => {
-  try {
-    const studentsWithParents = await Parent.distinct("student");
-
-    const studentsWithoutParents = await Student.find({
-      _id: { $nin: studentsWithParents },
-    });
-
-    res.status(200).json(studentsWithoutParents);
-  } catch (err) {
-    console.error("Error fetching students without parents:", err);
-    res.status(500).json({ message: "Server error" });
-  }
-});
+router.get("/no-parent", verifyToken, getStudentsWithoutParents);
 
 router.get("/", verifyToken, findAllGeneric(Student));
 router.get("/:id", verifyToken, findIdGeneric(Student));
